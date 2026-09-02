5ec7736b39e5fd2f13c2450077781f7fb3e33766
azaanyaq <azaanyaqub2@gmail.com>
2026-09-03 01:49:33 +0400

    Deleted examples because I forgot to

--- examples/nn_test.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from EduNet import demo_vis
-
-demo_vis(architecture=[2, 5, 5, 1], epochs=200, alpha=0.6, seed=None)
